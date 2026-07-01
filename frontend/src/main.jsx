51129a828ea4eed94f146cc124ee6e5f2c6d18c4
DESKTOP-IACQQJ6\Dev 1 <aditi.rao@outstripinfotech.com>
2026-07-01 13:41:43 -0500

    add chatbot backend live url

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import store, { persistor } from "./office_Managment_Frontent/store/Store";
-import Loader from "./office_Managment_Frontent/components/Loader/Loader";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
